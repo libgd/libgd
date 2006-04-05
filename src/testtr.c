f8f9265f026ddc48769a24db4ba665840195c2f1
pierre <none@none>
2006-04-05 15:53:57 +0000

    - sync to 2.0.28

--- src/testtr.c
+++ src/testtr.c
@@ -1,4 +1,5 @@
-#ifdef HAVE_CONFIG_H                                                  #include "config.h"
+#ifdef HAVE_CONFIG_H
+#include "config.h"
 #endif /* HAVE_CONFIG_H */
 
 #include <stdio.h>
