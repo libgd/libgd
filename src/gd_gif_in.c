b7da6fd579078699ceccc2117895e384b19b311a
mattias <none@none>
2007-08-07 19:53:25 +0000

    - file lost in last commit for #105 :)

--- src/gd_gif_in.c
+++ src/gd_gif_in.c
@@ -110,6 +110,8 @@ BGD_DECLARE(gdImagePtr) gdImageCreateFromGifPtr (int size, void *data)
 {
   gdImagePtr im;
   gdIOCtx *in = gdNewDynamicCtxEx (size, data, 0);
+  if(!in)
+  	return 0;
   im = gdImageCreateFromGifCtx (in);
   in->gd_free (in);
   return im;
