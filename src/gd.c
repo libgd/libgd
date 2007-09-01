841d43d812f97ad55dcc8408fa2039353895165a
mattias <none@none>
2007-09-01 12:35:18 +0000

    -MFB #112, More checking

--- src/gd.c
+++ src/gd.c
@@ -588,7 +588,7 @@ BGD_DECLARE(int) gdImageColorResolveAlpha (gdImagePtr im, int r, int g, int b, i
 
 BGD_DECLARE(void) gdImageColorDeallocate (gdImagePtr im, int color)
 {
-  if (im->trueColor || (color >= gdMaxColors))
+  if (im->trueColor || (color >= gdMaxColors) || (color < 0))
     {
       return;
     }
