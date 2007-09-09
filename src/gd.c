8dbae8d154b5cd545c40243ccc08d49bb1a35218
mattias <none@none>
2007-09-09 13:14:25 +0000

    -MFB, #113, gdImageColorTransparent can write outside buffer

--- src/gd.c
+++ src/gd.c
@@ -600,6 +600,9 @@ BGD_DECLARE(void) gdImageColorTransparent (gdImagePtr im, int color)
 {
   if (!im->trueColor)
     {
+	if((color < -1) || (color >= gdMaxColors)) {
+		return;
+	}
       if (im->transparent != -1)
 	{
 	  im->alpha[im->transparent] = gdAlphaOpaque;
