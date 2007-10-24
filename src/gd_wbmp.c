46fd62585ecbced255193cd5fe964bc44eb137de
scottmac <none@none>
2007-10-24 13:46:56 +0000

    - #127,  segfault when createwbmp fails

--- src/gd_wbmp.c
+++ src/gd_wbmp.c
@@ -107,9 +107,11 @@ BGD_DECLARE(void) gdImageWBMPCtx (gdImagePtr image, int fg, gdIOCtx * out)
 
 
   /* create the WBMP */
-  if ((wbmp =
-       createwbmp (gdImageSX (image), gdImageSY (image), WBMP_WHITE)) == NULL)
-    fprintf (stderr, "Could not create WBMP\n");
+  if ((wbmp = createwbmp (gdImageSX (image), gdImageSY (image), WBMP_WHITE)) == NULL)
+  {
+	fprintf (stderr, "Could not create WBMP\n");
+	return;
+  }
 
   /* fill up the WBMP structure */
   pos = 0;
