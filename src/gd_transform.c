141b38634296cfc1a95ce252b136ec67df0d05fd
pajoye <none@none>
2007-11-03 10:40:14 +0000

    - #129, Add flip functions, horizontally, vertically

---
+++ src/gd_transform.c
@@ -0,0 +1,73 @@
+#include "gd.h"
+
+BGD_DECLARE(int)
+gdImageFlipHorizontal(gdImagePtr im)
+{
+	int *p1, *p2, *src;
+	register int x, y, tmp;
+
+
+	if (im->trueColor) {
+		int p;
+		for (y = 0; y < im->sy / 2; y++) {
+			for (x = 0; x < im->sx; x++) {
+				p = im->tpixels[y][x];
+				im->tpixels[y][x] =	im->tpixels[im->sy - 1 - y][x];
+				im->tpixels[im->sy - 1 - y][x] = p;
+			}
+		}
+	} else {
+		unsigned char p;
+		for (y = 0; y < im->sy / 2; y++) {
+			for (x = 0; x < im->sx; x++) {
+				p = im->tpixels[y][x];
+				im->tpixels[y][x] =	im->tpixels[im->sy - 1 - y][x];
+				im->tpixels[im->sy - 1 - y][x] = p;
+			}
+		}
+
+	}
+	return 1;
+}
+
+BGD_DECLARE(int)
+gdImageFlipVertical(gdImagePtr im)
+{
+
+	int x, y;
+	int w = im->sx;
+	int h = im->sy;
+
+	if (im->trueColor) {
+		int p;
+
+		for (y = 0; y < im->sy; y++) {
+			for (x = 0; x < im->sx / 2; x++) {
+				p = im->tpixels[y][x];
+				im->tpixels[y][x] =	im->tpixels[y][im->sx - 1 - x];
+				im->tpixels[y][im->sx - 1 - x] = p;
+			}
+		}
+	} else {
+		unsigned char p;
+
+		for (y = 0; y < im->sy; y++) {
+			for (x = 0; x < im->sx / 2; x++) {
+				p = im->pixels[y][x];
+				im->pixels[y][x] =	im->pixels[y][im->sx - 1 - x];
+				im->pixels[y][im->sx - 1 - x] = p;
+			}
+		}
+	}
+}
+
+BGD_DECLARE(int)
+gdImageFlipBoth(gdImagePtr im)
+{
+	int res;
+
+	res = gdImageFlipVertical(im);
+	res &= gdImageFlipHorizontal(im);
+	return res;
+}
+
