0d971891828cc15712aec19ccf13949c823c0c75
mattias <none@none>
2007-12-22 12:59:42 +0000

    - ws + cs

--- src/gdcache.h
+++ src/gdcache.h
@@ -45,48 +45,49 @@ extern "C" {
 /*********************************************************/
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#	include "config.h"
 #endif
 
 #include <stdlib.h>
 #ifndef NULL
-#define NULL (void *)0
+#	define NULL (void *)0
 #endif
 
 /* user defined function templates */
-typedef int (*gdCacheTestFn_t) (void *userdata, void *keydata);
-typedef void *(*gdCacheFetchFn_t) (char **error, void *keydata);
-typedef void (*gdCacheReleaseFn_t) (void *userdata);
+typedef int (*gdCacheTestFn_t)(void *userdata, void *keydata);
+typedef void *(*gdCacheFetchFn_t)(char **error, void *keydata);
+typedef void (*gdCacheReleaseFn_t)(void *userdata);
 
 /* element structure */
 typedef struct gdCache_element_s gdCache_element_t;
 struct gdCache_element_s
 {
-  gdCache_element_t *next;
-  void *userdata;
+	gdCache_element_t *next;
+	void *userdata;
 };
 
 /* head structure */
 typedef struct gdCache_head_s gdCache_head_t;
 struct gdCache_head_s
 {
-  gdCache_element_t *mru;
-  int size;
-  char *error;
-  gdCacheTestFn_t gdCacheTest;
-  gdCacheFetchFn_t gdCacheFetch;
-  gdCacheReleaseFn_t gdCacheRelease;
+	gdCache_element_t *mru;
+	int size;
+	char *error;
+	gdCacheTestFn_t gdCacheTest;
+	gdCacheFetchFn_t gdCacheFetch;
+	gdCacheReleaseFn_t gdCacheRelease;
 };
 
 /* function templates */
-gdCache_head_t *gdCacheCreate (int size,
-			       gdCacheTestFn_t gdCacheTest,
-			       gdCacheFetchFn_t gdCacheFetch,
-			       gdCacheReleaseFn_t gdCacheRelease);
+gdCache_head_t *gdCacheCreate(int size,
+	gdCacheTestFn_t gdCacheTest,
+	gdCacheFetchFn_t gdCacheFetch,
+	gdCacheReleaseFn_t gdCacheRelease
+);
 
-void gdCacheDelete (gdCache_head_t * head);
+void gdCacheDelete(gdCache_head_t *head);
 
-void *gdCacheGet (gdCache_head_t * head, void *keydata);
+void *gdCacheGet(gdCache_head_t *head, void *keydata);
 
 #ifdef __cplusplus
 }
