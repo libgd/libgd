67c1ccbd4d06900f361d30aa64188be4763b1132
pajoye <none@none>
2008-03-14 21:39:13 +0000

    - add arc example

---
+++ examples/arc.c
@@ -0,0 +1,29 @@
+/* $Id$ */
+#include "gd.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ 	gdImagePtr im, im2;
+	FILE *fp;
+	char path[2048];
+	int i;
+	char dst[2048];
+	int cor_rad = 60;
+	im = gdImageCreateTrueColor(400, 400);
+	gdImageFilledRectangle(im, 0, 0, 399, 399, 0x00FFFFFF);
+
+	gdImageFilledArc (im, cor_rad, 399 - cor_rad, cor_rad *2, cor_rad *2, 90, 180, 0x0, gdPie);
+
+	fp = fopen("b.png", "wb");
+	if (!fp) {
+		fprintf(stderr, "Can't save png image.\n");
+		gdImageDestroy(im);
+		return 1;
+	}
+	gdImagePng(im, fp);
+	fclose(fp);
+
+	gdImageDestroy(im);
+}
