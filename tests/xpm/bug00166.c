156b6d90f10734e7f45af22b7ab692f4b9daf0aa
tabe <none@none>
2008-08-14 10:45:42 +0000

    (supplement) fixed FS#166: * added test subdirectory 'xpm'

---
+++ tests/xpm/bug00166.c
@@ -0,0 +1,29 @@
+/* $Id$ */
+#include "gd.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "gdtest.h"
+
+int
+main(int argc, char *argv[])
+{
+  gdImagePtr im;
+  char path[1024];
+  int c, result;
+
+  sprintf(path, "%s/xpm/bug00166.xpm", GDTEST_TOP_DIR);
+  im = gdImageCreateFromXpm(path);
+  if (!im) {
+    return 2;
+  }
+  c = gdImageGetPixel(im, 1, 1);
+  if (gdImageRed(im, c)      == 0xAA
+      && gdImageGreen(im, c) == 0xBB
+      && gdImageBlue(im, c)  == 0xCC) {
+    result = 0;
+  } else {
+    result = 1;
+  }
+  gdImageDestroy(im);
+  return result;
+}
