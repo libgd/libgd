d16e4941d5a1374bba0056e9ce87222cd2fe2b4c
tabe <none@none>
2008-08-21 11:34:05 +0000

    * recognize the color names like 'AliceBlue' in gdImageCreateFromXpm() * deleted unused codes

---
+++ tests/xpm/color_name.c
@@ -0,0 +1,29 @@
+/* $Id$ */
+#include "gd.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "gdtest.h"
+
+int
+main(int argc, char *argv[])
+{
+  gdImagePtr im;
+  char path[1024];
+  int c, result;
+
+  sprintf(path, "%s/xpm/color_name.xpm", GDTEST_TOP_DIR);
+  im = gdImageCreateFromXpm(path);
+  if (!im) {
+    return 2;
+  }
+  c = gdImageGetPixel(im, 2, 2);
+  if (gdImageRed(im, c)      == 0xFF
+      && gdImageGreen(im, c) == 0xFF
+      && gdImageBlue(im, c)  == 0x0) {
+    result = 0;
+  } else {
+    result = 1;
+  }
+  gdImageDestroy(im);
+  return result;
+}
