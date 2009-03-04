02ae7c20079a64e8c809108deb1a0d50d816c9dd
tabe <none@none>
2009-03-04 11:55:18 +0000

    check whether a new image is null or not.

--- src/gd_crop.c
+++ src/gd_crop.c
@@ -19,6 +19,7 @@ BGD_DECLARE(gdImagePtr) gdImageCrop(gdImagePtr src, const gdRect *crop)
 	gdImagePtr dst;
 
 	dst = gdImageCreateTrueColor(crop->width, crop->height);
+	if (!dst) return NULL;
 	gdImageCopy(dst, src, 0, 0, crop->x, crop->y, crop->width, crop->height);
 
 	return dst;
