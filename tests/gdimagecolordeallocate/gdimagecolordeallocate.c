0560e0f6ff8e3e26f901d6399f31cf02977f0587
Takeshi Abe <tabe@fixedpoint.jp>
2013-03-06 12:15:03 +0900

    fix test for gdImageColorDeallocate

--- tests/gdimagecolordeallocate/gdimagecolordeallocate.c
+++ tests/gdimagecolordeallocate/gdimagecolordeallocate.c
@@ -5,27 +5,25 @@
 int main()
 {
 	gdImagePtr im;
-	int error = 0;
+	int c;
 
 	im = gdImageCreate(1, 1);
-
-	im->open[gdMaxColors] = 0;
-
-	gdImageColorDeallocate(im, gdMaxColors);
-
-	if(im->open[gdMaxColors] == 1) {
-		error = -1;
-	}	
-
-	im->open[-1] = 0;
-
-	gdImageColorDeallocate(im, -1);
-
-	if(im->open[-1] == 1) {
-		error = -1;
+	/* test for deallocating a color */
+	c = gdImageColorAllocate(im, 255, 255, 255);
+	if (c < 0) {
+		gdImageDestroy(im);
+		return 1;
+	}
+	gdImageColorDeallocate(im, c);
+	if (!im->open[c]) {
+		gdImageDestroy(im);
+		return 1;
 	}
 
+	/* just see whether it is OK with out-of-bounds values */
+	gdImageColorDeallocate(im, gdMaxColors);
+	gdImageColorDeallocate(im, -1);
 	gdImageDestroy(im);
 
-	return error;
+	return 0;
 }
