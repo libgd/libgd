769fb1534c878e827a3cbb9cc89241b31f394bfa
Chris Reuter <chris@blit.ca>
2013-11-11 16:38:58 -0500

    Added another Makefile.
    This change adds a Makefile and bash script for building and testing
    libgd on Windows with MinGW and MSYS.  This was written after I
    concluded that it was easier and faster to just write a new Makefile
    from scratch than to get autotools or CMake to play nicely with my
    Windows installation.

---
+++ windows/msys/run_tests.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+# Parameters
+CFLAGS_EXTRA=$1     # Extra C flags
+    
+
+LOG=run_tests.log
+
+CFLAGS="-g -Igdtest/ -I. -I../src/ -D_WIN32 $CFLAGS_EXTRA"
+LDFLAGS='-L../src -llibgd.3.0.1'
+DLLPATH=../src:/c/Progra~1/GnuWin32/bin
+
+function run_gcc {
+    if msg=`gcc $* 2>&1`; then
+        true
+    else
+        echo "COMMAND: gcc $*" >> $LOG
+        echo $msg >> $LOG
+        false
+    fi
+}
+
+# Switch to the working directory
+PATH=$PATH:$DLLPATH
+cd ../../tests
+
+# Initial setup
+echo "Setting up..."
+[ -f $LOG ] && rm -f $LOG
+[ -f test_config.h ] || echo '#define GDTEST_TOP_DIR "."' > test_config.h
+run_gcc -c $CFLAGS gdtest/gdtest.c
+
+
+echo "Running tests:"
+count=0
+failures=0
+compile_failures=0
+for test in `find . -name \*.c | grep -v '^./gdtest'`; do
+    count=`expr $count + 1`
+
+    exe=${test%.c}.exe
+    if run_gcc -o $exe $CFLAGS $LDFLAGS $test gdtest.o; then
+        true;
+    else
+        echo "COMPILE_FAIL: $test"
+        compile_failures=`expr $compile_failures + 1`
+        continue
+    fi
+    
+    echo "Running $exe:" >> $LOG
+    if $exe 2>&1 >> $LOG; then
+        echo "PASS: $test"
+    else
+        failures=`expr $failures + 1`
+        echo "FAIL: $test"
+    fi
+    echo >> $LOG
+done
+
+echo "$failures failures and $compile_failures compile failures out of $count tests."
+echo "Error messages in $LOG"
+
+
+
