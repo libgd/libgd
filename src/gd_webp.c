e3e72d5253831ad7dac85d0de2bd16fcf61a2015
Wangkun <wangkun611@gmail.com>
2015-05-07 15:02:37 +0800

    add gdImageCreateFromWebpPtr impl

--- src/gd_webp.c
+++ src/gd_webp.c
@@ -26,6 +26,18 @@ gdImagePtr gdImageCreateFromWebp (FILE * inFile)
 	return im;
 }
 
+
+gdImagePtr gdImageCreateFromWebpPtr (int size, void *data)
+{
+	gdImagePtr im;
+	gdIOCtx *in = gdNewDynamicCtxEx(size, data, 0);
+	if (!in)
+		return 0;
+	im = gdImageCreateFromWebpCtx (in);
+	in->gd_free(in);
+	return im;
+}
+
 gdImagePtr gdImageCreateFromWebpCtx (gdIOCtx * infile)
 {
 	int    width, height;
