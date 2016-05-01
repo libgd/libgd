92c3209484cb8baa07c88f65f4be0de8ce238de0
Mike Frysinger <vapier@gentoo.org>
2016-04-30 20:47:50 -0400

    tests: fix distclean errors

---
+++ tests/gdhelpers.c
@@ -0,0 +1,6 @@
+/*
+ * Dummy file to workaround an automake limitation: if we try to list the
+ * file directly, automake will clean up its internal state in both the src/
+ * and tests/ subdirs, but only one will succeed.
+ */
+#include "../src/gdhelpers.c"
