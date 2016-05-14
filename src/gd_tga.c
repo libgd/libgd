2733b623ee08eca6b277ad8b0b89251462c09435
Mike Frysinger <vapier@gentoo.org>
2016-05-14 03:51:26 -0400

    tga: cleanup/simplify a bit
    Try to standardize the internals a little bit and simplify the read
    paths to abort early on when we know we won't actually handle the
    input format.  Avoid resetting pointers that are on the stack or we
    release as it's a waste of code.

--- src/gd_tga.c
+++ src/gd_tga.c
@@ -61,7 +61,7 @@ BGD_DECLARE(gdImagePtr) gdImageCreateFromTgaCtx(gdIOCtx* ctx)
 	tga->bitmap = NULL;
 	tga->ident = NULL;
 
-	if (!read_header_tga(ctx, tga)) {
+	if (read_header_tga(ctx, tga) < 0) {
 		free_tga(tga);
 		return NULL;
 	}
@@ -71,7 +71,7 @@ BGD_DECLARE(gdImagePtr) gdImageCreateFromTgaCtx(gdIOCtx* ctx)
 		image_block_size = (tga->width * tga->height) * pixel_block_size;
 	*/
 
-	if (read_image_tga(ctx, tga)) {
+	if (read_image_tga(ctx, tga) < 0) {
 		free_tga(tga);
 		return NULL;
 	}
@@ -215,32 +215,23 @@ int read_image_tga( gdIOCtx *ctx, oTga *tga )
 		return -1;
 	}
 
+	/*! \todo Add more image type support.
+	 */
+	if (tga->imagetype != TGA_TYPE_RGB && tga->imagetype != TGA_TYPE_RGB_RLE)
+		return -1;
+
 	/*!	\brief Allocate memmory for image block
 	 *  Allocate a chunk of memory for the image block to be passed into.
 	 */
 	tga->bitmap = (int *) gdMalloc(image_block_size * sizeof(int));
-	if (tga->bitmap == NULL) {
+	if (tga->bitmap == NULL)
 		return -1;
-	}
 
-	/*! \todo Add image type support
-	 *  Add support for this image type.
-	 */
-	if (tga->imagetype == TGA_TYPE_INDEXED) {
-		return -1;
-	}
-
-	/*! \todo Add image type support
-	 *  Add support for this image type.
-	 */
-	if (tga->imagetype == TGA_TYPE_INDEXED_RLE) {
-		return -1;
-	}
-
-	/*! \brief Read in uncompressed RGB TGA
-	 *  Chunk load the pixel data from an uncompressed RGB type TGA.
-	 */
-	if (tga->imagetype == TGA_TYPE_RGB) {
+	switch (tga->imagetype) {
+	case TGA_TYPE_RGB:
+		/*! \brief Read in uncompressed RGB TGA
+		 *  Chunk load the pixel data from an uncompressed RGB type TGA.
+		 */
 		conversion_buffer = (unsigned char *) gdMalloc(image_block_size * sizeof(unsigned char));
 		if (conversion_buffer == NULL) {
 			return -1;
@@ -254,12 +245,12 @@ int read_image_tga( gdIOCtx *ctx, oTga *tga )
 		}
 
 		gdFree(conversion_buffer);
-	}
+		break;
 
-	/*! \brief Read in RLE compressed RGB TGA
-	 *  Chunk load the pixel data from an RLE compressed RGB type TGA.
-	 */
-	if (tga->imagetype == TGA_TYPE_RGB_RLE) {
+	case TGA_TYPE_RGB_RLE:
+		/*! \brief Read in RLE compressed RGB TGA
+		 *  Chunk load the pixel data from an RLE compressed RGB type TGA.
+		 */
 		decompression_buffer = (uint8_t*) gdMalloc(image_block_size * sizeof(uint8_t));
 		if (decompression_buffer == NULL) {
 			return -1;
@@ -307,23 +298,10 @@ int read_image_tga( gdIOCtx *ctx, oTga *tga )
 		}
 		gdFree( decompression_buffer );
 		gdFree( conversion_buffer );
+		break;
 	}
 
-	/*!	\todo Add image type support
-	 *  Add support for this image type.
-	 */
-	if( tga->imagetype == TGA_TYPE_GREYSCALE ) {
-		return -1;
-	}
-
-	/*!	\todo Add image type support
-	 *  Add support for this image type.
-	 */
-	if( tga->imagetype == TGA_TYPE_GREYSCALE_RLE ) {
-		return -1;
-	}
-
-	return 0;
+	return 1;
 }
 
 /*!	\brief Cleans up a TGA structure.
@@ -333,15 +311,10 @@ int read_image_tga( gdIOCtx *ctx, oTga *tga )
 void free_tga(oTga * tga)
 {
 	if (tga) {
-		if (tga->ident) {
+		if (tga->ident)
 			gdFree(tga->ident);
-			tga->ident = NULL;
-		}
-		if (tga->bitmap) {
+		if (tga->bitmap)
 			gdFree(tga->bitmap);
-			tga->bitmap = NULL;
-		}
 		gdFree(tga);
-		tga = NULL;
 	}
 }
