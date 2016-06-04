4de4d09fa147346b6849d418e94c101e1582825f
Pierre Joye <pierre.php@gmail.com>
2016-06-05 00:33:33 +0700

    unused var

--- tests/gdimagefilltoborder/github_bug_215.c
+++ tests/gdimagefilltoborder/github_bug_215.c
@@ -6,8 +6,6 @@ int main() {
 	gdImagePtr im;
 
 	im = gdImageCreate(50, 50);
-	white = gdImageColorAllocate(im, 255, 255, 255);
-	red = gdImageColorAllocate(im, 255, 0, 0);
 	gdImageFillToBorder(im, 0, 0, 1024, 1024);
 	gdImageDestroy(im);
 	return 0;
