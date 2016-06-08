6156e6645d39cbd59b0339d41f73fdb1208fcddb
Pierre Joye <pierre.php@gmail.com>
2016-06-08 11:21:42 +0700

    fix #215, invalid color index, missing case for invalid border

--- tests/gdimagefilltoborder/github_bug_215.c
+++ tests/gdimagefilltoborder/github_bug_215.c
@@ -4,9 +4,16 @@
 
 int main() {
 	gdImagePtr im;
-
+	int bgd, color, border;
 	im = gdImageCreate(50, 50);
-	gdImageFillToBorder(im, 0, 0, 1024, 1024);
+	bgd = gdImageColorAllocate(im, 255, 0, 0);
+	color = gdImageColorAllocate(im, 255, 0, 0);
+	border = gdImageColorAllocate(im, 255, 0, 0);
+	gdImageFillToBorder(im, 0, 0, border + 10, color);
+	gdImageFillToBorder(im, 0, 0, -border, color);
+	gdImageFillToBorder(im, 0, 0, border, color + 10);
+	gdImageFillToBorder(im, 0, 0, border, -color);
+	gdImageFillToBorder(im, 0, 0, border + 10, color + 10);
 	gdImageDestroy(im);
 	return 0;
 }
