19788a221ad529dea4962cc858a9e0ecbb8f5bda
Pierre Joye <pierre.php@gmail.com>
2016-06-09 10:50:30 +0700

    fix #172 single line rectangle creates wriclers. Simplify test.

--- tests/gdimagerectangle/github_bug_00172.c
+++ tests/gdimagerectangle/github_bug_00172.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include "gd.h"
-#include "gdfontg.h"
+
 #include "gdtest.h"
 
 int main() {
-	gdImagePtr im, imres, imexp;
-	int white, black, red;
-	char path[1024];
+	gdImagePtr im;
+	int black, white;
 	int error = 0;
-
-	im = gdImageCreate(400, 200);
+	int xs = 300, xe = 350, i;
+	im = gdImageCreateTrueColor(400, 200);
 
 	if (im == NULL) {
 		gdTestErrorMsg("gdImageCreate failed.\n");
@@ -18,28 +17,35 @@ int main() {
 
 	white = gdImageColorAllocate(im, 255,255,255);
 	black = gdImageColorAllocate(im, 0,0,0);
-	red = gdImageColorAllocate(im, 255,0,0);
-	gdImageString(im, gdFontGetGiant(), 5, 5, "RECTANGLES", black);
-	gdImageString(im, gdFontGetGiant(), 150, 5, "Filled", black);
-	gdImageString(im, gdFontGetGiant(), 300, 5, "Unfilled", black);
-	gdImageString(im, gdFontGetGiant(), 5, 75, "Height=0", black);
-	gdImageString(im, gdFontGetGiant(), 5, 145, "Width=0", black);
-	gdImageFilledRectangle(im, 150, 95, 200, 95, black);
-	gdImageRectangle(im, 300, 95, 350, 95, black);
-	gdImageSetPixel(im, 300, 94, red);
-	gdImageSetPixel(im, 350, 94, red);
 
-	gdImageFilledRectangle(im, 175, 125, 175, 175, black);
-	gdImageRectangle(im, 325, 125, 325, 175, black);
-	gdImageSetPixel(im, 300, 94, red);
-	gdImageSetPixel(im, 350, 94, red);
-	gdImageEllipse(im, 325, 95, 90, 50, red);
+	gdImageFilledRectangle(im, 0, 0, 400,200, white);
 
+	gdImageRectangle(im, xs, 95, xe, 95, black);
 
+	for (i = xs; i <= xe; i++) {
+		int c = gdImageGetPixel(im, i, 94);
+		if (c != white) {
+			error |=1;
+		}
+	}
+	for (i = xs; i <= xe; i++) {
+		int c = gdImageGetPixel(im, i, 95);
+		if (c != black) {
+			error |=1;
+		}
+	}
+	for (i = xs; i <= xe; i++) {
+		int c = gdImageGetPixel(im, i, 96);
+		if (c != white) {
+			error |=1;
+		}
+	}
 
-	sprintf(path, "%s/gdimagerectangle/%s", GDTEST_TOP_DIR, "github_bug_00172_exp.png");
-	if (!gdAssertImageEqualsToFile(path, im)) {
-		error |= 1;
+	if (gdImageGetPixel(im, xs -1, 95) != white) {
+		error |=1;
+	}
+	if (gdImageGetPixel(im, xe +1, 95) != white) {
+		error |=1;
 	}
 	gdImageDestroy(im);
 
