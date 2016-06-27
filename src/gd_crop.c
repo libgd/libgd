46f2c690c682d5da33539034480c58c1a6d5cfac
Pierre Joye <pierre.php@gmail.com>
2016-06-27 11:38:07 +0700

    fix php 72494, CID 149753, color is unsigned int, remove useless <0 comparison

--- src/gd_crop.c
+++ src/gd_crop.c
@@ -136,7 +136,7 @@ BGD_DECLARE(gdImagePtr) gdImageCropThreshold(gdImagePtr im, const unsigned int c
 		return NULL;
 	}
 
-	if (color < 0 || (!gdImageTrueColor(im) && color >= gdImageColorsTotal(im))) {
+	if (!gdImageTrueColor(im) && color >= gdImageColorsTotal(im)) {
 		return NULL;
 	}
 
