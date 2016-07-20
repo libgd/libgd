libgd/libgd

commit 0dd1706c14abced200f8e5f83d2f86dc44cd9508

possible null deref

src/gd_interpolation.c
@@ -1055,7 +1055,7 @@ gdImageScaleTwoPass(const gdImagePtr src, const unsigned int new_width,
         scale_pass_res = _gdScalePass(tmp_im, src_height, dst, new_height, new_width, VERTICAL);
 		if (scale_pass_res != 1) {
 			gdImageDestroy(dst);
-			if (src != tmp_im && tmp_im != NULL) {
+			if (tmp != NULL && src != tmp_im) {
 				gdImageDestroy(tmp_im);
 			}
 			return NULL;

commit 8b7e76dd370e068515162e74986f7968dddc7384

Fix typo introduced in 0dd1706

src/gd_interpolation.c
@@ -1055,7 +1055,7 @@ gdImageScaleTwoPass(const gdImagePtr src, const unsigned int new_width,
         scale_pass_res = _gdScalePass(tmp_im, src_height, dst, new_height, new_width, VERTICAL);
 		if (scale_pass_res != 1) {
 			gdImageDestroy(dst);
-			if (tmp != NULL && src != tmp_im) {
+			if (tmp_im != NULL && src != tmp_im) {
 				gdImageDestroy(tmp_im);
 			}
 			return NULL;