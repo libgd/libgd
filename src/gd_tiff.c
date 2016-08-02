e59e0574bef03ea37de452417ac444b286688825
Christoph M. Becker <cmbecker69@gmx.de>
2016-08-02 12:10:33 +0200

    Fix invalid read in gdImageCreateFromTiffPtr()
    tiff_invalid_read.tiff is corrupt, and causes an invalid read in
    gdImageCreateFromTiffPtr(), but not in gdImageCreateFromTiff(). The culprit
    is dynamicGetbuf(), which doesn't check for out-of-bound reads. In this case,
    dynamicGetbuf() is called with a negative dp->pos, but also positive buffer
    overflows have to be handled, in which case 0 has to be returned (cf. commit
    75e29a9).
    
    Fixing dynamicGetbuf() exhibits that the corrupt TIFF would still create
    the image, because the return value of TIFFReadRGBAImage() is not checked.
    We do that, and let createFromTiffRgba() fail if TIFFReadRGBAImage() fails.
    
    This issue had been reported by Ibrahim El-Sayed to security@libgd.org.
    
    CVE-2016-6911

--- src/gd_tiff.c
+++ src/gd_tiff.c
@@ -759,6 +759,7 @@ static int createFromTiffRgba(TIFF * tif, gdImagePtr im)
 	int height = im->sy;
 	uint32 *buffer;
 	uint32 rgba;
+	int success;
 
 	/* switch off colour merging on target gd image just while we write out
 	 * content - we want to preserve the alpha data until the user chooses
@@ -771,18 +772,20 @@ static int createFromTiffRgba(TIFF * tif, gdImagePtr im)
 		return GD_FAILURE;
 	}
 
-	TIFFReadRGBAImage(tif, width, height, buffer, 0);
+	success = TIFFReadRGBAImage(tif, width, height, buffer, 1);
 
-	for(y = 0; y < height; y++) {
-		for(x = 0; x < width; x++) {
-			/* if it doesn't already exist, allocate a new colour,
-			 * else use existing one */
-			rgba = buffer[(y * width + x)];
-			a = (0xff - TIFFGetA(rgba)) / 2;
-			color = gdTrueColorAlpha(TIFFGetR(rgba), TIFFGetG(rgba), TIFFGetB(rgba), a);
-
-			/* set pixel colour to this colour */
-			gdImageSetPixel(im, x, height - y - 1, color);
+	if (success) {
+		for(y = 0; y < height; y++) {
+			for(x = 0; x < width; x++) {
+				/* if it doesn't already exist, allocate a new colour,
+				 * else use existing one */
+				rgba = buffer[(y * width + x)];
+				a = (0xff - TIFFGetA(rgba)) / 2;
+				color = gdTrueColorAlpha(TIFFGetR(rgba), TIFFGetG(rgba), TIFFGetB(rgba), a);
+	
+				/* set pixel colour to this colour */
+				gdImageSetPixel(im, x, height - y - 1, color);
+			}
 		}
 	}
 
@@ -790,7 +793,7 @@ static int createFromTiffRgba(TIFF * tif, gdImagePtr im)
 
 	/* now reset colour merge for alpha blending routines */
 	gdImageAlphaBlending(im, alphaBlendingFlag);
-	return GD_SUCCESS;
+	return success;
 }
 
 /*
