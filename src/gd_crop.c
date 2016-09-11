16aaa033db9e03d8283c26fb7bcd5d2b52549dda
Christoph M. Becker <cmbecker69@gmx.de>
2016-09-11 16:17:49 +0200

    Fix #297: gdImageCrop() converts palette image to truecolor image
    When just cropping an image, we want to stick with the same image type.
    
    (cherry picked from commit a59523038d1a80a3701ca78f719beb0668f31b81)

--- src/gd_crop.c
+++ src/gd_crop.c
@@ -25,7 +25,11 @@ BGD_DECLARE(gdImagePtr) gdImageCrop(gdImagePtr src, const gdRect *crop)
 {
 	gdImagePtr dst;
 
-	dst = gdImageCreateTrueColor(crop->width, crop->height);
+	if (gdImageTrueColor(src)) {
+		dst = gdImageCreateTrueColor(crop->width, crop->height);
+	} else {
+		dst = gdImageCreate(crop->width, crop->height);
+	}
 	if (!dst) return NULL;
 	gdImageCopy(dst, src, 0, 0, crop->x, crop->y, crop->width, crop->height);
 
