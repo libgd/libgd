a74909b9a43a46518cac18eefba8809e712e210e
Christoph M. Becker <cmbecker69@gmx.de>
2016-09-21 00:15:47 +0200

    Fix #307: GD_QUANT_NEUQUANT fails to unset trueColor flag
    We must unset the trueColor image after converting it; gdImageCopy() wont't
    (rightly) do that for us.

--- src/gd_topal.c
+++ src/gd_topal.c
@@ -1550,6 +1550,7 @@ static int gdImageTrueColorToPaletteBody (gdImagePtr oim, int dither, int colors
 			return FALSE;
 		} else {
 			gdImageCopy(oim, nim, 0, 0, 0, 0, oim->sx, oim->sy);
+			oim->trueColor = 0;
 			gdImageDestroy(nim);
 		}
 		return TRUE;
---
+++ tests/gdimagetruecolortopalette/bug00307.c
@@ -0,0 +1,26 @@
+/**
+ * Regression test for <https://github.com/libgd/libgd/issues/307>
+ *
+ * We're testing that an image that has been converted to palette with
+ * GD_QUANT_NEUQUANT has its trueColor flag unset.
+ */
+
+ 
+#include "gd.h"
+#include "gdtest.h"
+
+
+int main()
+{
+	gdImagePtr im;
+
+	im = gdImageCreateTrueColor(100, 100);
+
+	gdTestAssert(gdImageTrueColorToPaletteSetMethod(im, GD_QUANT_NEUQUANT, 0));
+	gdImageTrueColorToPalette(im, 0, 256);
+	gdTestAssert(!gdImageTrueColor(im));
+
+	gdImageDestroy(im);
+
+	return gdNumFailures();
+}
