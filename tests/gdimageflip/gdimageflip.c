d6d2dec0fba0c92dfcf3d1c480ba2c41bae74853
Christoph M. Becker <cmbecker69@gmx.de>
2016-10-02 14:36:09 +0200

    Test and document gdImageFlip*()

---
+++ tests/gdimageflip/gdimageflip.c
@@ -0,0 +1,37 @@
+/**
+ * Testing the basic operation of the gdImageFlip*() functions
+ */
+
+
+#include "gd.h"
+#include "gdtest.h"
+
+
+static void test_flip(void (*func)(gdImagePtr), const char *filename)
+{
+    gdImagePtr im;
+    FILE *fp;
+    char *path;
+
+    fp = gdTestFileOpen2("gdimageflip", "remi.png");
+    im = gdImageCreateFromPng(fp);
+    fclose(fp);
+
+    func(im);
+
+    path = gdTestFilePath2("gdimageflip", filename);
+    gdAssertImageEqualsToFile(path, im);
+    gdFree(path);
+
+    gdImageDestroy(im);
+}
+
+
+int main()
+{
+    test_flip(gdImageFlipVertical, "gdimageflipvertical_exp.png");
+    test_flip(gdImageFlipHorizontal, "gdimagefliphorizontal_exp.png");
+    test_flip(gdImageFlipBoth, "gdimageflipboth_exp.png");
+
+    return gdNumFailures();
+}
