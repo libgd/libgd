1846f48e5fcdde996e7c27a4bbac5d0aef183e4b
Christoph M. Becker <cmbecker69@gmx.de>
2016-11-12 15:00:31 +0100

    Fix #340: System frozen
    gdImageCreate() doesn't check for oversized images and as such is prone
    to DoS vulnerabilities. We fix that by applying the same overflow check
    that is already in place for gdImageCreateTrueColor().
    
    CVE-2016-9317

--- src/gd.c
+++ src/gd.c
@@ -188,6 +188,7 @@ BGD_DECLARE(gdImagePtr) gdImageCreate (int sx, int sy)
 	if (overflow2(sx, sy)) {
 		return NULL;
 	}
+
 	if (overflow2(sizeof (unsigned char *), sy)) {
 		return NULL;
 	}
---
+++ tests/gdimagecreate/bug00340.c
@@ -0,0 +1,33 @@
+/**
+ * Regression test for <https://github.com/libgd/libgd/issues/340>
+ *
+ * We're testing that trying to create an oversized image fails early,
+ * triggering an appropriate warning.
+ */
+
+
+#include <string.h>
+#include "gd.h"
+#include "gd_errors.h"
+#include "gdtest.h"
+
+
+#define MSG "product of memory allocation multiplication would exceed INT_MAX, failing operation gracefully\n"
+
+
+void error_handler(int priority, const char *format, ...)
+{
+    gdTestAssert(priority == GD_WARNING);
+    gdTestAssert(!strcmp(format, MSG));
+}
+
+
+int main()
+{
+    gdImagePtr im;
+
+    im = gdImageCreate(64970, 65111);
+    gdTestAssert(im == NULL);
+
+    return gdNumFailures();
+}
