4abd8147f1c09b453c0690394abcc09156f18fde
Christoph M. Becker <cmbecker69@gmx.de>
2016-11-16 18:10:29 +0100

    Fix #338: Fatal and normal libjpeg/ibpng errors not distinguishable
    libgd clients need to be able to distinguish between fatal and
    "extremely fatal" libjpeg and libpng errors, because in the former case
    execution can proceed, but in the latter case libgd calls exit().
    Therefore we report fatal errors as GD_WARNING.
    
    (cherry picked from commit fea55903eeeb89d4a0117519f8c1d039adc95da5)

---
+++ tests/jpeg/bug00338.c
@@ -0,0 +1,51 @@
+/**
+ * Regression test for <https://github.com/libgd/libgd/issues/338>
+ *
+ * We're testing that reading a JPEG image with gdImageCreateFromJpeg()
+ * raises a GD_WARNING for the fatal libjpeg error, but not a GD_ERROR.
+ * We also make sure, that the fatal libjpeg error is actually reported.
+ *
+ * See also ../png/bug00338.c
+ */
+
+
+#include <string.h>
+#include "gd.h"
+#include "gd_errors.h"
+#include "gdtest.h"
+
+
+#define MSG "gd-jpeg: JPEG library reports unrecoverable error: %s"
+
+
+static int error_handler_called = 0;
+
+
+static void error_handler(int priority, const char *format, va_list args)
+{
+    if (!strcmp(format, MSG)) {
+        gdTestAssertMsg(priority == GD_WARNING, "expected priority %d, but got %d", GD_WARNING, priority);
+        error_handler_called = 1;
+    }
+}
+
+
+int main()
+{
+    gdImagePtr im;
+    FILE *fp;
+
+    gdSetErrorMethod(error_handler);
+
+    im = gdImageCreateTrueColor(10, 10);
+    fp = gdTestTempFp();
+    gdImagePng(im, fp);
+    gdImageDestroy(im);
+
+    im = gdImageCreateFromJpeg(fp);
+    gdTestAssert(im == NULL);
+
+    gdTestAssert(error_handler_called);
+
+    return gdNumFailures();
+}
