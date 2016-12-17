69d2fd2c597ffc0c217de1238b9bf4d4bceba8e6
Christoph M. Becker <cmbecker69@gmx.de>
2016-12-17 17:06:58 +0100

    Fix #354: Signed Integer Overflow gd_io.c
    GD2 stores the number of horizontal and vertical chunks as words (i.e. 2
    byte unsigned). These values are multiplied and assigned to an int when
    reading the image, what can cause integer overflows. We have to avoid
    that, and also make sure that either chunk count is actually greater
    than zero. If illegal chunk counts are detected, we bail out from
    reading the image.

---
+++ tests/gd2/bug00354.c
@@ -0,0 +1,32 @@
+/**
+ * We're testing GD2 image files which report illegal chunk counts. These should
+ * not cause integer overflows or other issues, but instead simply fail to be
+ * loaded.
+ *
+ * See also <https://github.com/libgd/libgd/issues/354>.
+ */
+
+
+#include "gd.h"
+#include "gdtest.h"
+
+
+int main()
+{
+    gdImagePtr im;
+    FILE *fp;
+
+    fp = gdTestFileOpen2("gd2", "bug00354a.gd2");
+    gdTestAssert(fp != NULL);
+    im = gdImageCreateFromGd2(fp);
+    gdTestAssert(im == NULL);
+    fclose(fp);
+
+    fp = gdTestFileOpen2("gd2", "bug00354b.gd2");
+    gdTestAssert(fp != NULL);
+    im = gdImageCreateFromGd2(fp);
+    gdTestAssert(im == NULL);
+    fclose(fp);
+
+    return gdNumFailures();
+}
