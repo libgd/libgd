34183698e1e7b2ae7f4648c0a25dbd2cfdb23c0a
Christoph M. Becker <cmbecker69@gmx.de>
2017-01-25 13:37:39 +0100

    Add test summary for Windows
    (cherry picked from commit 1f136c5f169fafbbb017704724505370ae97c6ae)

--- windows/run_tests.bat
+++ windows/run_tests.bat
@@ -4,6 +4,11 @@ if [%PLATFORM%]==[] set PLATFORM=x86
 if [%PREFERREDTOOLARCHITECTURE%]==[x64] set PLATFORM=x64
 set PLATFORM=%PLATFORM:X=x%
 
+set total=0
+set total_passed=0
+set total_failed=0
+set total_missing=0
+
 for /f %%T in (testlist.txt) do (
   set test=%%T
   set output=test: !test!....................................................................
@@ -20,9 +25,19 @@ for /f %%T in (testlist.txt) do (
   rem if defined testerr echo error: !testerr!
   rem if defined testout echo msg: !testout!
   
+  set /a total=total+1
+  if "!result!"=="Passed" set /a total_passed=total_passed+1
+  if "!result!"=="Failed" set /a total_failed=total_failed+1
+  if "!result!"=="NotFound" set /a total_missing=total_missing+1
+
   if defined APPVEYOR (
     appveyor AddTest "!test! %PLATFORM%" -Outcome !result! -Framework Custom -Filename %%~nT.exe -Duration 0
   )
 )
+echo.
+echo Tests:   !total!
+echo Passed:  !total_passed!
+echo Failed:  !total_failed!
+echo Missing: !total_missing!
 
 exit /b 0
