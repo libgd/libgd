4b0f3724022ad5bdf16d86409126436ff5a31afb
willson-chen <willson.chenwx@gmail.com>
2019-12-13 04:26:40 +0100

    Fixed #369: fix new_a init error in gdImageConvolution()

--- src/gd_filter.c
+++ src/gd_filter.c
@@ -537,6 +537,7 @@ BGD_DECLARE(int) gdImageConvolution(gdImagePtr src, float filter[3][3], float fi
 	for ( y=0; y<src->sy; y++) {
 		for(x=0; x<src->sx; x++) {
 			new_r = new_g = new_b = 0;
+			pxl = f(srcback, x, y);
 			new_a = gdImageAlpha(srcback, pxl);
 
 			for (j=0; j<3; j++) {
