9088591eae437358ee5b929adf82865e37e3001e
Christoph M. Becker <cmbecker69@gmx.de>
2020-01-04 23:43:47 +0100

    Fix #585: gdTransformAffineCopy() changes interpolation method
    We have to properly initialize `interpolation_id_bak`.

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -75,8 +75,14 @@
 /* Define to 1 if you have the <stdint.h> header file. */
 #cmakedefine HAVE_STDINT_H
 
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H
+
 /* Define to 1 if you have the <strings.h> header file. */
 #cmakedefine HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #cmakedefine HAVE_SYS_STAT_H
