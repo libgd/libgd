3dd0e308cbd2c24fde2fc9e9b707181252a2de95
Christoph M. Becker <cmbecker69@gmx.de>
2020-05-05 12:02:45 +0200

    Fix #615: gdImageStringFT() fails for empty strings as of libgd 2.3.0 (#633)
    We change the return type of `textLayout()` to `ssize_t`, and signal
    failure by returning `-1`, so that laying out an empty string is no
    longer handled as failure.  We make sure that no overflow occurs,
    assuming that all `int` values can be fully represented as `ssize_t`.

---
+++ tests/gdimagestringft/bug00615.c
@@ -0,0 +1,25 @@
+/**
+ * Test that rendering an empty string does not fail
+ *
+ * Rendering an empty string with gdImageStringFT() is not supposed to fail;
+ * it is just a no-op.
+ *
+ * See <https://github.com/libgd/libgd/issues/615>
+ */
+
+#include "gd.h"
+#include "gdtest.h"
+
+int main()
+{
+    gdImagePtr im = gdImageCreate(100, 100);
+
+    int rect[8];
+    int fg = gdImageColorAllocate(im, 255, 255, 255);
+    char *path = gdTestFilePath("freetype/DejaVuSans.ttf");
+    char *res = gdImageStringFT(im, rect, fg, path, 12, 0, 10, 10, "");
+
+    gdTestAssert(res == NULL);
+
+    return gdNumFailures();
+}
