ccd512f02674a03f45dd6206b96a25a00bb41c72
sterlingpickens <sterling_pickens@yahoo.com>
2021-04-29 04:12:52 -0700

    entities.py cleanups WIP

--- src/entities.h
+++ src/entities.h
@@ -1,27 +1,27 @@
-/*
+ /*
  * Generated file - do not edit directly.
  * This file was generated from:
  *     https://html.spec.whatwg.org/entities.json
  * by means of entities.py in the libgd src dir
-*/
+ */
 #ifndef GD_ENTITIES_H
+#define GD_ENTITIES_H 1
 
 #include <stdlib.h>
 #include <stdint.h>
 
-//html entity strings are entity prefix + string + suffix limited
-//hex and dec should be limited to current unicode spec + entity prefix + suffix
-#define GD_ENTITIES_H 1
-#define NR_OF_ENTITIES 2125
-#define ENTITY_NAME_LENGTH_MAX 33
-#define ENTITY_HEX_LENGTH_MAX 10
-#define ENTITY_DEC_LENGTH_MAX 10
+// html entity strings are entity prefix + string + suffix limited
+// hex and dec should be limited to current unicode spec + entity prefix + suffix
 
-struct entities_s{
+struct entities_s {
                   	char *name;
                         uint32_t unicode_a;
                         uint32_t unicode_b;
                 };
 extern const struct entities_s entities[NR_OF_ENTITIES];
 
+#define NR_OF_ENTITIES 2125
+#define ENTITY_NAME_LENGTH_MAX 33
+#define ENTITY_HEX_LENGTH_MAX 10
+#define ENTITY_DEC_LENGTH_MAX 10
 #endif
--- src/entities.py
+++ src/entities.py
@@ -1,93 +1,104 @@
 #!/usr/bin/env python3
 import json
 import re
+import urllib.request
+import os
+import os.path
+from pathlib import Path
 
-### Declare headers
-header_a = """/*
+SRCDIR = Path(__file__).resolve().parent
+os.chdir(SRCDIR)
+
+# Declare headers
+HEADER_C_FILE = """ /*
  * Generated file - do not edit directly.
  * This file was generated from:
  *     https://html.spec.whatwg.org/entities.json
  * by means of entities.py in the libgd src dir
-*/
-"""
+ */
 
-header_b = """
 #include "entities.h"
 
-const struct entities_s entities[NR_OF_ENTITIES] = {"""
-
-header_c = """/*
+const struct entities_s entities[NR_OF_ENTITIES] = {
+"""
+HEADER_H_FILE = """ /*
  * Generated file - do not edit directly.
  * This file was generated from:
  *     https://html.spec.whatwg.org/entities.json
  * by means of entities.py in the libgd src dir
-*/
+ */
 #ifndef GD_ENTITIES_H
+#define GD_ENTITIES_H 1
 
 #include <stdlib.h>
 #include <stdint.h>
 
-//html entity strings are entity prefix + string + suffix limited
-//hex and dec should be limited to current unicode spec + entity prefix + suffix
-"""
+// html entity strings are entity prefix + string + suffix limited
+// hex and dec should be limited to current unicode spec + entity prefix + suffix
 
-header_d = """
-struct entities_s{
+struct entities_s {
                   	char *name;
                         uint32_t unicode_a;
                         uint32_t unicode_b;
                 };
 extern const struct entities_s entities[NR_OF_ENTITIES];
 
-#endif
 """
 
-### Open files
-file_json = open("entities.json", mode="r")
+# Open files
+if not os.path.exists("entities.json"):
+    print("entities.json not found, attempting to download...")
+    with open("entities.json", "wb") as file_json:
+        file_json.write(urllib.request.urlopen("https://html.spec.whatwg.org/entities.json").read())
+        file_json.close()
+        print("    successful")
+
+with open("entities.json", "rb") as file_json:
+    entities = json.load(file_json)
+
 file_ent_h = open("entities.h", mode="w")
 file_ent_c = open("entities.c", mode="w")
 
-### Initialize some objects
-x = json.load(file_json)
+# Initialize some objects
 curline = 1
 totallines = 0
 len_name_max = 0
-p = re.compile("&\S+;")
+name_matcher = re.compile("&\S+;")
 
-### Print file headers
-file_ent_c.write(header_a + header_b + "\n")
-file_ent_h.write(header_c)
-file_ent_h.write("#define GD_ENTITIES_H 1\n")
+# Print file headers
+file_ent_c.write(HEADER_C_FILE)
+file_ent_h.write(HEADER_H_FILE)
 
-### Count how many matching lines we have
-for e in x:
-    if p.match(e):
+# Count how many matching lines we have
+for key in entities:
+    if name_matcher.match(key):
         totallines+=1
-### Print json entities to struct
-for e in x:
-    if p.match(e):
-        if len(e) > len_name_max:
-            len_name_max = len(e)
-        string = "\t\t\t" + "{\"" + e.replace("&", "") + "\", "
+# Print json entities to struct
+for key in entities:
+    if name_matcher.match(key):
+        if len(key) > len_name_max:
+            len_name_max = len(key)
+        string = "\t" + "{\"" + key.replace("&", "") + "\", "
         file_ent_c.write(string.replace(";", ""))
-        if len(x[e]["codepoints"]) > 1:
-            string = str(x[e]["codepoints"][0]) + ", " + str(x[e]["codepoints"][1]) + "}"
+        codepoints = entities[key]["codepoints"]
+        if len(codepoints) > 1:
+            string = str(codepoints[0]) + ", " + str(codepoints[1]) + "}"
         else:
-            string = str(x[e]["codepoints"][0]) + ", 0}"
+            string = str(codepoints[0]) + ", 0}"
 
         if curline == totallines:
             file_ent_c.write(string + "\n")
         else:
             file_ent_c.write(string + "," + "\n")
         curline+=1
-### Print file ends
-file_ent_c.write("\t\t\t};" + "\n")
-file_ent_h.write("#define NR_OF_ENTITIES " + str(totallines) + "\n");
-file_ent_h.write("#define ENTITY_NAME_LENGTH_MAX " + str(len_name_max) + "\n");
-file_ent_h.write("#define ENTITY_HEX_LENGTH_MAX 10\n")
-file_ent_h.write("#define ENTITY_DEC_LENGTH_MAX 10\n")
-file_ent_h.write(header_d)
-### Close files
+# Print file ends
+file_ent_c.write("\t};")
+file_ent_h.write(f"#define NR_OF_ENTITIES {totallines}\n");
+file_ent_h.write(f"#define ENTITY_NAME_LENGTH_MAX {len_name_max}\n");
+file_ent_h.write(f"#define ENTITY_HEX_LENGTH_MAX 10\n")
+file_ent_h.write(f"#define ENTITY_DEC_LENGTH_MAX 10\n#endif")
+# Close files
 file_json.close()
 file_ent_h.close()
 file_ent_c.close()
+print("entities.h & entities.c updated from entities.json")
