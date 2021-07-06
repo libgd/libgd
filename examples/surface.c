4a4d8c9471e72eb2a2c282b1536a2a67dc8e55c8
Pierre Joye <pierre.php@gmail.com>
2021-07-06 11:39:13 +0700

    just add G&B for completeness example

--- examples/surface.c
+++ examples/surface.c
@@ -27,12 +27,24 @@ int main()
             img[x] = 0x80FF0000;
         }
     }
+    for (int y = 200; y < 350; y++) {
+        unsigned int* img = (unsigned int*)(surface->data + surface->stride * y);
+        for (int x = 200; x < 350; x++) {
+            img[x] = 0xFF00FF00;
+        }
+    }
+    for (int y = 400; y < 450; y++) {
+        unsigned int* img = (unsigned int*)(surface->data + surface->stride * y);
+        for (int x = 400; x < 450; x++) {
+            img[x] = 0x300000FF;
+        }
+    }
 	if (!surface) {
 		fprintf(stderr, "Can't create 400x400 surface\n");
 		return 1;
 	}
 
-	save_png(surface, "surface1.png");
+	save_png(surface, "surface.png");
 
 	gdSurfaceDestroy(surface);
 	return 0;
