bdc281eadb1d58d5c0c7bbc1125ee4674256df08
Pierre Joye <pierre.php@gmail.com>
2021-08-25 15:28:32 +0700

    Fix #318, these macros are not used as planed, we have separate functions for each

--- src/gd.h
+++ src/gd.h
@@ -1604,11 +1604,6 @@ BGD_DECLARE(void) gdImageFlipHorizontal(gdImagePtr im);
 BGD_DECLARE(void) gdImageFlipVertical(gdImagePtr im);
 BGD_DECLARE(void) gdImageFlipBoth(gdImagePtr im);
 
-#define GD_FLIP_HORINZONTAL 1 /* typo, kept for BC */
-#define GD_FLIP_HORIZONTAL 1
-#define GD_FLIP_VERTICAL 2
-#define GD_FLIP_BOTH 3
-
 /**
  * Group: Crop
  *
