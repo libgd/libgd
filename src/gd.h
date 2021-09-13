f4bc1f5c26925548662946ed7cfa473c190a104a
Remi Collet <remi@remirepo.net>
2021-09-13 14:57:52 +0200

    Revert "Fix #318, these macros are not used as planed, we have separate functions for each"
    This reverts commit bdc281eadb1d58d5c0c7bbc1125ee4674256df08.

--- src/gd.h
+++ src/gd.h
@@ -1604,6 +1604,11 @@ BGD_DECLARE(void) gdImageFlipHorizontal(gdImagePtr im);
 BGD_DECLARE(void) gdImageFlipVertical(gdImagePtr im);
 BGD_DECLARE(void) gdImageFlipBoth(gdImagePtr im);
 
+#define GD_FLIP_HORINZONTAL 1 /* typo, kept for BC */
+#define GD_FLIP_HORIZONTAL 1
+#define GD_FLIP_VERTICAL 2
+#define GD_FLIP_BOTH 3
+
 /**
  * Group: Crop
  *
