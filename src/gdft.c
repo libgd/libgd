0f046d341de4c4897bd9d97db822987388187839
Pierre Joye <pierre.php@gmail.com>
2022-01-30 11:02:37 +0700

    Fix #808, free info. Not sure what happens, it keeps being removed on cherry-pick to 2.3....

--- src/gdft.c
+++ src/gdft.c
@@ -1433,9 +1433,7 @@ BGD_DECLARE(char *) gdImageStringFTEx (gdImagePtr im, int *brect, int fg, const 
 					gdFree(text);
 					gdCacheDelete (tc_cache);
 					gdMutexUnlock (gdFontCacheMutex);
-					if (info) {
-						gdFree(info);
-					}
+					gdFree(info);
 					return "Problem allocating memory";
 				}
 				xshow_pos = 0;
@@ -1448,9 +1446,7 @@ BGD_DECLARE(char *) gdImageStringFTEx (gdImagePtr im, int *brect, int fg, const 
 					gdFree(text);
 					gdCacheDelete (tc_cache);
 					gdMutexUnlock (gdFontCacheMutex);
-					if (info) {
-						gdFree(info);
-					}
+					gdFree(info);
 					return "Problem allocating memory";
 				}
 			}
@@ -1467,6 +1463,7 @@ BGD_DECLARE(char *) gdImageStringFTEx (gdImagePtr im, int *brect, int fg, const 
 			gdFree(text);
 			gdCacheDelete (tc_cache);
 			gdMutexUnlock (gdFontCacheMutex);
+			gdFree(info);
 			return "Problem loading glyph";
 		}
 
