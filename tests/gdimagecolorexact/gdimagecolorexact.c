910924ffd78fd9ae13e27e2136316f8e49f4fcb1
Pierre Joye <pierre.php@gmail.com>
2022-02-01 17:37:49 +0700

    Bug/818 (#820)
    * Partial #818, unused arg
    
    * Partial #818, init var
    
    * partail #818, fix va_args usage
    
    * partail #818, handle f* calls and avoid possible call to malloc with negative values
    
    * partail #818, prevent double free
    
    * partail #818, resource leak if test fail
    
    * partail #818, null deref fix
    
    * partail #818, avoid double free on fp failure
    
    * Partial #818, fix error msg
    
    * Partial #818, leak on error
    
    * Partial #818, null deref
    
    * Partial #818, avoid possible negative index on failure
    
    * partial #818, does not free if we return if requested new size overflow
    
    * partial #818, avoid double free, free where the alloc happened
    
    * partial #818, fix assert logic and test exp_size for <=0
    
    * partial #818, fix assert logic for color idx test

--- tests/gdimagecolorexact/gdimagecolorexact.c
+++ tests/gdimagecolorexact/gdimagecolorexact.c
@@ -30,29 +30,29 @@ int main()
 	c3 = gdImageColorExactAlpha(im, 255, 0, 255, 100);
 	c4 = gdImageColorExactAlpha(im, 255, 34, 255, 100);
 
-
-	if (gdTestAssert(c1 == 0) != 1) {
+	if (gdTestAssert(c1 == 0)) {
 		color = gdTrueColorAlpha(gdImageRed(im, c1), gdImageGreen(im, c1),
 								gdImageBlue(im, c1), 0);
 		if (gdTestAssert(color == 0xFF00FF) != 1) {
 			error = -1;
 		}
 	}
-	if (gdTestAssert(c2 == 1) != 1) {
+
+	if (gdTestAssert(c2 == 1)) {
 		color = gdTrueColorAlpha(gdImageRed(im, c2), gdImageGreen(im, c2),
 								gdImageBlue(im, c2), 0);
 		if (gdTestAssert(color == 0xFFC800) != 1) {
 			error = -1;
 		}
 	}
-	if (gdTestAssert(c3 == 2) != 1) {
+	if (gdTestAssert(c3 == 2)) {
 		color = gdTrueColorAlpha(gdImageRed(im, c3), gdImageGreen(im, c3),
 								gdImageBlue(im, c3), 0);
 		if (gdTestAssert(color == 0xFF00FF) != 1) {
 			error = -1;
 		}
 	}
-	if (gdTestAssert(c4 == -1) != 1) {
+	if (!gdTestAssert(c4 == -1)) {
 		error = -1;
 	}
 
