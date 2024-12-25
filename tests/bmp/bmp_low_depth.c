79acb54ff7f6117df72431ba40b2f5db55c0ae8d
Christoph M. Becker <cmbecker69@gmx.de>
2024-12-25 15:17:16 +0100

    Transparent support for writing 1bpp and 4bpp BMPs
    As is, we write palette images as 8bpp BMP images.  Besides that this
    is wasteful, some readers may not even be able to read 8bpp BMP images.
    Thus, we write palette images with at most two colors as 1bpp BMPs,
    unless compression is requested, because BMPs do not support compressed
    encoding of 1bpp images; we write palette images with at most 16 colors
    as 4bpp BMPs, and apply `BMP_BI_RLE4` compression if requested.
    
    We piggy-back on the existing implementation for `BMP_BI_RLE8`; that
    keeps it simple, and although it might not yield the best compression,
    it ought to be good enough.
    
    References:
    <https://en.wikipedia.org/wiki/BMP_file_format>
    <https://learn.microsoft.com/en-us/windows/win32/gdi/bitmap-compression>
    
    Closes #911.

---
+++ tests/bmp/bmp_low_depth.c
@@ -0,0 +1,67 @@
+#include "gd.h"
+#include "gdtest.h"
+
+int get_bpp(unsigned char *data)
+{
+    return data[0x1d] << 8 | data[0x1c];
+}
+
+int get_comp(unsigned char *data)
+{
+    return data[0x21] << 24 | data[0x20] << 16 | data[0x1f] << 8 | data[0x1e];
+}
+
+int main()
+{
+    gdImagePtr im1, im2;
+    int fg, r, g, b;
+    int size;
+    unsigned char *data;
+
+    /* 2 color image */
+    im1 = gdImageCreate(99, 99); /* odd pixels per row! */
+    gdImageColorAllocate(im1, 0, 0, 0); /* background */
+    fg = gdImageColorAllocate(im1, 255, 255, 255);
+    gdImageRectangle(im1, 0, 0, 98, 98, fg); /* border to verify edge case */
+    gdImageEllipse(im1, 49, 49, 75, 75, fg);
+
+    data = gdImageBmpPtr(im1, &size, 0);
+    gdTestAssert(get_bpp(data) == 1);
+    gdTestAssert(get_comp(data) == 0);
+    im2 = gdImageCreateFromBmpPtr(size, data);
+    gdAssertImageEquals(im1, im2);
+
+    data = gdImageBmpPtr(im1, &size, 1);
+    gdTestAssert(get_bpp(data) == 4);
+    gdTestAssert(get_comp(data) == 2);
+    im2 = gdImageCreateFromBmpPtr(size, data);
+    gdAssertImageEquals(im1, im2);
+
+    /* 5 color image */
+    r = gdImageColorAllocate(im1, 255, 0, 0);
+    g = gdImageColorAllocate(im1, 0, 255, 0);
+    b = gdImageColorAllocate(im1, 0, 0, 255);
+    gdImageEllipse(im1, 17, 17, 28, 28, r);
+    gdImageEllipse(im1, 49, 17, 28, 28, r);
+    gdImageEllipse(im1, 81, 17, 28, 28, r);
+    gdImageEllipse(im1, 17, 49, 28, 28, g);
+    gdImageEllipse(im1, 49, 49, 28, 28, g);
+    gdImageEllipse(im1, 81, 49, 28, 28, g);
+    gdImageEllipse(im1, 17, 81, 28, 28, b);
+    gdImageEllipse(im1, 49, 81, 28, 28, b);
+    gdImageEllipse(im1, 81, 81, 28, 28, b);
+
+    data = gdImageBmpPtr(im1, &size, 0);
+    gdTestAssert(get_bpp(data) == 4);
+    gdTestAssert(get_comp(data) == 0);
+    im2 = gdImageCreateFromBmpPtr(size, data);
+    gdAssertImageEquals(im1, im2);
+
+    data = gdImageBmpPtr(im1, &size, 1);
+    gdTestAssert(get_bpp(data) == 4);
+    gdTestAssert(get_comp(data) == 2);
+    im2 = gdImageCreateFromBmpPtr(size, data);
+    gdAssertImageEquals(im1, im2);
+
+    return gdNumFailures();
+}
