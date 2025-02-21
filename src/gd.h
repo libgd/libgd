2be005f311232bc3d8a544f73ce8049d2b2fb885
Art Kay <art@artkay.net>
2025-02-21 14:27:07 -0500

    Fix typo (repsented → represented) (#954)

--- src/gd.h
+++ src/gd.h
@@ -127,7 +127,7 @@ extern "C" {
    pixels are represented by integers, which
    must be 32 bits wide or more.
 
-   True colors are repsented as follows:
+   True colors are represented as follows:
 
    ARGB
 
