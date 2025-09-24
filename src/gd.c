2bc44ae1865af240a6feb9e57433397ddb82c63c
David Carlier <devnexen@gmail.com>
2025-09-24 06:16:21 +0100

    add new helper

--- src/gd.c
+++ src/gd.c
@@ -2214,7 +2214,7 @@ BGD_DECLARE(void) gdImageEllipse(gdImagePtr im, int mx, int my, int w, int h, in
 	bq = b * b;
 	dx = aq << 1;
 	dy = bq << 1;
-	if (bq > 0 && a > INT64_MAX / bq) {
+	if (overflow2_64(a, bq)) {
 		return;
 	}
 	r  = a * bq;
@@ -2269,7 +2269,7 @@ BGD_DECLARE(void) gdImageFilledEllipse (gdImagePtr im, int mx, int my, int w, in
 	bq = b * b;
 	dx = aq << 1;
 	dy = bq << 1;
-	if (bq > 0 && a > INT64_MAX / bq) {
+	if (overflow2_64(a, bq)) {
 		return;
 	}
 	r  = a * bq;
--- src/gd_security.c
+++ src/gd_security.c
@@ -14,6 +14,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <limits.h>
 #include "gd.h"
 #include "gd_errors.h"
@@ -26,6 +27,19 @@ int overflow2(int a, int b)
 	}
 	if(a > INT_MAX / b) {
 		gd_error_ex(GD_WARNING, "product of memory allocation multiplication would exceed INT_MAX, failing operation gracefully\n");
+		return 1;
+	}
+	return 0;
+}
+
+int overflow2_64(int64_t a, int64_t b)
+{
+	if(a <= 0 || b <= 0) {
+		gd_error_ex(GD_WARNING, "one parameter to a memory allocation multiplication is negative or zero, failing operation gracefully\n");
+		return 1;
+	}
+	if(a > INT64_MAX / b) {
+		gd_error_ex(GD_WARNING, "product of memory allocation multiplication would exceed INT64_MAX, failing operation gracefully\n");
 		return 1;
 	}
 	return 0;
